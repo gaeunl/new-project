fe8ccafae66b0cf7b5550e82e4f46be36eb7e8bb
Gaeun Lee <gaeun1065@gmail.com>
2021-05-22 21:27:06 -0700

    close #1; create http post request for the Person Object

--- server/src/main/java/com/project/server/api/PersonController.java
+++ server/src/main/java/com/project/server/api/PersonController.java
@@ -2,15 +2,24 @@ package com.project.server.api;
 
 import com.project.server.model.Person;
 import com.project.server.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
+@RestController
 public class PersonController {
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
-    public int addPerson(Person person){
+    @PostMapping
+    public int addPerson(@RequestBody Person person){
         return personService.addPerson(person);
     }
 }
