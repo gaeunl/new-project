fa840d30c680265946b9653abddfab4720829b61
Gaeun Lee <gaeun1065@gmail.com>
2021-05-23 11:57:32 -0700

    close #2

--- server/src/main/java/com/project/server/dao/PersonDao.java
+++ server/src/main/java/com/project/server/dao/PersonDao.java
@@ -2,12 +2,18 @@ package com.project.server.dao;
 
 import com.project.server.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
+
     int insertPerson(UUID id, Person person);
-    default int insertPerson(Person person){
+
+    default int insertPerson(Person person) {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
+
 }
--- server/src/main/java/com/project/server/dao/PersonDataAccessService.java
+++ server/src/main/java/com/project/server/dao/PersonDataAccessService.java
@@ -8,12 +8,17 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository("fakeDao")
-public class PersonDataAccessService implements PersonDao{
+public class PersonDataAccessService implements PersonDao {
     private static List<Person> DB = new ArrayList<>();
 
     @Override
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
